a6b2b95807d2930c050af82b9a417d0861f9eeca
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-13 20:55:46 -0300

    exercicio

---
+++ Fiap.Web.Aula02/Fiap.Web.Aula02/Controllers/VeiculoController.cs
@@ -0,0 +1,40 @@
+﻿using Fiap.Web.Aula02.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Web.Aula02.Controllers
+{
+    public class VeiculoController : Controller
+    {
+        //Lista para gravar os veiculos
+        private static IList<Veiculo> _lista = new List<Veiculo>();
+
+        //TAREFA!
+        //Cadastrar o veiculo na lista
+        //Usar o index para exibir todos os veiculos em uma tabela HTML
+
+        public IActionResult Index()
+        {
+            return View(_lista);
+        }
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Veiculo veiculo)
+        {
+            //Cadastrar na lista
+            _lista.Add(veiculo);
+
+            //Mostrar mensagem de sucesso
+            // Após cadastrar o veículo com sucesso
+            TempData["mostrarMensagem"] = true;
+            TempData["mensagem"] = "Veiculo cadastrado com SUCESSO!";
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Fiap.Web.Aula02/Fiap.Web.Aula02/Models/Veiculo.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fiap.Web.Aula02.Models
+{
+    public class Veiculo
+    {
+        public int Id { get; set; }
+        public string? Modelo { get; set; }
+        public string? Marca { get; set; }
+        [Display(Name ="Disponível")]
+        public bool Disponivel { get; set; }
+        public long Km { get; set; }
+        [Display(Name ="Ano de Fabricação")]
+        public int Ano { get; set; }
+        public string? Placa { get; set; }
+        [Display(Name = "Categoria do Veículo")]
+        public TipoVeiculo Tipo { get; set; }
+    }
+
+    public enum TipoVeiculo
+    {
+        SUV, SEDAN, HATCH, SPORT, UTILITARIO
+    }
+}
