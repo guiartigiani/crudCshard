6caffc40fd674673c20d6cde4ab9692fde9d4729
Guilherme Artigiani <82193572+guiartigiani@users.noreply.github.com>
2023-09-15 00:00:12 -0300

    Crud Completo

--- Fiap.Web.Aula02/Fiap.Web.Aula02/Controllers/VeiculoController.cs
+++ Fiap.Web.Aula02/Fiap.Web.Aula02/Controllers/VeiculoController.cs
@@ -7,10 +7,7 @@ namespace Fiap.Web.Aula02.Controllers
     {
         //Lista para gravar os veiculos
         private static IList<Veiculo> _lista = new List<Veiculo>();
-
-        //TAREFA!
-        //Cadastrar o veiculo na lista
-        //Usar o index para exibir todos os veiculos em uma tabela HTML
+        private static int _id = 0;
 
         public IActionResult Index()
         {
@@ -26,6 +23,7 @@ namespace Fiap.Web.Aula02.Controllers
         [HttpPost]
         public IActionResult Cadastrar(Veiculo veiculo)
         {
+            veiculo.Id = ++_id;
             //Cadastrar na lista
             _lista.Add(veiculo);
 
@@ -34,6 +32,43 @@ namespace Fiap.Web.Aula02.Controllers
             TempData["mostrarMensagem"] = true;
             TempData["mensagem"] = "Veiculo cadastrado com SUCESSO!";
 
+            return RedirectToAction("Cadastrar");
+        }
+
+        [HttpGet]
+        public IActionResult Atualizar(int id)
+        {
+            var veiculo = _lista.First(v => v.Id == id);
+            return View(veiculo);
+        }
+
+        [HttpPost]
+        public IActionResult Atualizar(Veiculo veiculo)
+        {
+            var index = _lista.ToList().FindIndex(v => v.Id == veiculo.Id);
+            _lista[index] = veiculo;
+
+            TempData["mostrarMensagem"] = true;
+            TempData["mensagem"] = "Veiculo atualizado com SUCESSO!";
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Deletar(int id)
+        {
+            var veiculo = _lista.First(v => v.Id == id);
+            //_lista.Remove(veiculo);
+            return View(veiculo);
+            //return RedirectToAction("Index");
+        }
+
+        [HttpPost, ActionName("Deletar")]
+        public IActionResult Deletar(Veiculo veiculo, int id)
+        {
+            veiculo = _lista.First(v => v.Id == id);
+            _lista.Remove(veiculo);
+            //return View(veiculo);
             return RedirectToAction("Index");
         }
     }
